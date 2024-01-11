65c8a6f1499e382c7e0c81f220c41c77505c1aba
Ben Worth <u3243222@uni.canberra.edu.au>
2024-01-11 11:30:50 +1100

    first commit

---
+++ sence_poser/pose_menu.py
@@ -0,0 +1,98 @@
+import rclpy
+from rclpy.node import Node
+from math import pi
+
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+
+jointNames = ['fl_joint1', 'fl_joint2', 'fl_joint3',
+              'fr_joint1', 'fr_joint2', 'fr_joint3',
+              'bl_joint1', 'bl_joint2', 'bl_joint3',
+              'br_joint1', 'br_joint2', 'br_joint3']
+flatPose = [0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0]
+curlPose = [pi/2, pi/2, 2.0,
+            -pi/2, -pi/2, 2.0,
+            -pi/2, -pi/2, 2.0,
+            pi/2, pi/2, pi/2]
+crabStandPose = [-pi/2, pi/8, pi/6,
+                 pi/2, -pi/8, pi/6,
+                 pi/2, -pi/8, pi/6,
+                 -pi/2, pi/8, pi/6]
+
+class TrajectoryPublisher(Node):
+    def __init__(self):
+        super().__init__('trajectory_publisher')
+        self.publisher_ = self.create_publisher(JointTrajectory, 'joint_trajectory_controller/joint_trajectory', 10)
+        self.pose = flatPose
+        self.poseTime = 1
+
+    def poseMenu(self):
+        while (True):
+            try:
+                print("""sence pose publisher
+                time to pose = {ttp} seconds
+                1: flat
+                2: curl
+                3: crab stand
+                9: set time to pose
+                0: quit""".format(ttp=self.poseTime)
+                )
+                response = int(input("enter a number: "))
+                match response:
+                    case 1:
+                        self.pose = flatPose
+                        self.publishPose()
+                    case 2:
+                        self.pose = curlPose
+                        self.publishPose()
+                    case 3:
+                        self.pose = crabStandPose
+                        self.publishPose()
+                    case 9:
+                        self.changeSpeed()
+                    case 0:
+                        break;
+                    case _:
+                        print("unknown response")
+
+            except Exception as e:
+                print(e)
+
+    def publishPose(self):
+        msg = JointTrajectory()
+        msg.joint_names = jointNames
+        point1 = JointTrajectoryPoint()
+        point1.positions = self.pose
+        point1.time_from_start.sec = self.poseTime
+        msg.points.append(point1)
+
+        self.publisher_.publish(msg)
+        self.get_logger().info('Publishing now')
+
+    def changeSpeed(self):
+        try:
+            self.poseTime = int(input("enter value in seconds: "))
+        except Exception as e:
+            print(e)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    trajectory_publisher = TrajectoryPublisher()
+    #rclpy.spin(trajectory_publisher)
+
+    trajectory_publisher.poseMenu()
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    trajectory_publisher.destroy_node()
+
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'sence_poser'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Ben Worth',
+    maintainer_email='u3243222@uni.canberra.edu.au',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+                'pose_menu = sence_poser.pose_menu:main',
+        ],
+    },
+)
