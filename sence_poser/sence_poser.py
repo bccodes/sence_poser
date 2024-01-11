bece2391acce2c0b6645c74004b0c7316d60ca6f
Ben Worth <u3243222@uni.canberra.edu.au>
2024-01-11 18:38:20 +1100

    random changes, walking is bad

--- sence_poser/sence_poser.py
+++ sence_poser/sence_poser.py
@@ -1,7 +1,6 @@
 import rclpy
 from rclpy.node import Node
-from math import pi
-from time import sleep
+from .poses import *
 
 from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 
@@ -9,72 +8,52 @@ jointNames = ['fl_joint1', 'fl_joint2', 'fl_joint3',
               'fr_joint1', 'fr_joint2', 'fr_joint3',
               'bl_joint1', 'bl_joint2', 'bl_joint3',
               'br_joint1', 'br_joint2', 'br_joint3']
-flatPose = [0.0, 0.0, 0.0,
-            0.0, 0.0, 0.0,
-            0.0, 0.0, 0.0,
-            0.0, 0.0, 0.0]
-curlPose = [pi/2, pi/2, 2.0,
-            -pi/2, -pi/2, 2.0,
-            -pi/2, -pi/2, 2.0,
-            pi/2, pi/2, pi/2]
-crabStandPose = [-pi/2, pi/8, pi/6,
-                 pi/2, -pi/8, pi/6,
-                 pi/2, -pi/8, pi/6,
-                 -pi/2, pi/8, pi/6]
-dogReadyPose = [pi/2, 0.0, 0.0,
-               -pi/2, 0.0, 0.0,
-               -pi/2, 0.0, 0.0,
-               pi/2, 0.0, 0.0]
-dogFlatPose = [1.4, -pi/2, 0.2,
-               -1.4, pi/2, 0.2,
-               -1.8, -1.3, 0.2,
-               1.8, 1.3, 0.2]
-dogStandPose = [-0.9, -pi/2, 1.9,
-               0.9, pi/2, 1.9,
-               -4.0, -pi/2, 1.9,
-               4.0, pi/2, 1.9]
 
 class TrajectoryPublisher(Node):
     def __init__(self):
         super().__init__('trajectory_publisher')
         self.publisher_ = self.create_publisher(JointTrajectory, 'joint_trajectory_controller/joint_trajectory', 10)
-        self.pose = flatPose
         self.poseTime = 1
 
     def poseMenu(self):
         while (True):
             try:
                 print("""sence pose publisher
-                time to pose = {ttp} seconds
-                1: flat
-                2: curl
-                3: crab stand
-                4: dog ready
-                5: dog flat
-                6: dog stand
-                9: set time to pose
-                0: quit""".format(ttp=self.poseTime)
+    1: flat
+    2: curl
+    3: crab stand
+    4: dog ready
+    5: dog flat
+    6: dog stand
+    7: dog walk
+    9: set time to pose (currently {ttp}sec)
+    0: quit""".format(ttp=self.poseTime)
                 )
                 response = int(input("enter a number: "))
                 match response:
                     case 1:
-                        self.pose = flatPose
-                        self.publishPose()
+                        self.publishPose(flatPose)
                     case 2:
-                        self.pose = curlPose
-                        self.publishPose()
+                        self.publishPose(curlPose)
                     case 3:
-                        self.pose = crabStandPose
-                        self.publishPose()
+                        self.publishPose(crabStandPose)
                     case 4:
-                        self.pose = dogReadyPose
-                        self.publishPose()
+                        self.publishPose(dogReadyPose)
                     case 5:
-                        self.pose = dogFlatPose
-                        self.publishPose()
+                        self.publishPose(dogFlatPose)
                     case 6:
-                        self.pose = dogStandPose
-                        self.publishPose()
+                        self.publishPose(dogStandPose)
+                    case 7:
+                        step = 0
+                        while (True):
+                            try:
+                                for stepPose in dogWalkPoses:
+                                    self.publishPose(stepPose)
+                                    print(f"step {step}")
+                                    step += 1
+                                    input()
+                            except KeyboardInterrupt:
+                                pass
                     case 9:
                         self.changeSpeed()
                     case 0:
@@ -85,11 +64,11 @@ class TrajectoryPublisher(Node):
             except Exception as e:
                 print(e)
 
-    def publishPose(self):
+    def publishPose(self, goal):
         msg = JointTrajectory()
         msg.joint_names = jointNames
         point1 = JointTrajectoryPoint()
-        point1.positions = self.pose
+        point1.positions = goal
         point1.time_from_start.sec = self.poseTime
         msg.points.append(point1)
 
